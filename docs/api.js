3ec71976838bfbf53fe6dd5dd6829277526f724f
smasala <me@smasala.com>
2014-11-06 10:08:00 +0100

    docs api

---
+++ docs/api.js
@@ -0,0 +1,43 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Boot",
+        "Classes",
+        "Controllers",
+        "Data",
+        "Data.Store",
+        "Events",
+        "Firebrick",
+        "Languages",
+        "Router",
+        "Templates",
+        "Utils",
+        "Views",
+        "class.Base",
+        "controller.Base",
+        "store.Base",
+        "view.Base",
+        "window"
+    ],
+    "modules": [
+        "Firebrick",
+        "Firebrick.class",
+        "Global"
+    ],
+    "allModules": [
+        {
+            "displayName": "Firebrick",
+            "name": "Firebrick",
+            "description": "A lightweight JavaScript MVC Framework powered with jQuery, Bootstrap, Knockout JS and Require JS"
+        },
+        {
+            "displayName": "Firebrick.class",
+            "name": "Firebrick.class"
+        },
+        {
+            "displayName": "Global",
+            "name": "Global"
+        }
+    ]
+} };
+});
